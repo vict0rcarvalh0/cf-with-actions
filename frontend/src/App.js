6188463d236eecfe2ee8ee682f99ef8908dbf0d2
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2024-05-14 10:32:15 -0300

    feat(frontend): :sparkles: adding react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Inteli - Instituto de Tecnologia e Liderança</h1>
+      <p>Já se fazem faculdades como futuramente.</p>
+    </div>
+  );
+}
+
+export default App;
